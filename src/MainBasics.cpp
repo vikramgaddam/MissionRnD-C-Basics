d0f1b981872cb15826f3e36a8c5a04864d05c345
vikramgaddam <vikramgaddam@missionrnd.com>
2016-03-03 06:24:22 -0800

    Submit Commit

--- src/MainBasics.cpp
+++ src/MainBasics.cpp
@@ -23,34 +23,35 @@ int main(){
 	
 	//Test Sum of numbers
 	
-	int sumTemp = sum(4, 3);
-	printf("%d\n", sumTemp);
+	//int sumTemp = sum(4, 3);
+	//printf("%d\n", sumTemp);
 	
 	//Test Count file
 	
-	int countTemp = count(5, 20);
-	printf("%d\n", countTemp);
+	//int countTemp = count(5, 20);
+	//printf("%d\n", countTemp);
 
 
 	//Test Swap
 	int a = 5, b = 7;
 	swap(&a, &b);
 	printf("%d %d\n", a, b);
+	getchar();
 
 	//Is Older 
-	char date1[20] = "29-02-2004";
-	char date2[20] = "29-02-2000";
-	int check=isOlder(date1, date2);
-	if (check == 0){
-		printf("Both are of same age \n");
-	}
-	else{
-		printf("%d person is Older \n", check);
-	}
+	//char date1[20] = "29-02-2004";
+	//char date2[20] = "29-02-2000";
+	//int check=isOlder(date1, date2);
+	//if (check == 0){
+		//printf("Both are of same age \n");
+	//}
+	//else{
+		//printf("%d person is Older \n", check);
+	//}
 
 	//Variable Arguments
-	int total = variableArguments(3, 20, 90, 98);
-	printf("There are %d students greater than 90\n", total);
+	//int total = variableArguments(3, 20, 90, 98);
+	//printf("There are %d students greater than 90\n", total);
 	
 	
 	return 0;
